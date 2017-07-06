b6c06012885651ac87cbb65649a7a132b4acfaaa
cfen <chris.fenn@guardian.co.uk>
2017-07-06 16:11:06 +0100

    Added lists by age and status at top

--- src/render.js
+++ src/render.js
@@ -25,10 +25,11 @@ function formatData(data) {
     output.map((obj) => {
         obj.ref = count;
         obj.formatName = obj.name.split(",")[0];
-        obj.sortName = obj.family_name+obj.formatName;
+        obj.sortName = obj.family_name + obj.formatName;
         obj.sortOn = obj.family_name.charAt(0).toUpperCase();
         !obj.age ? obj.age = "unknown" : obj.age = obj.age.toString();
-        !obj.status ? obj.status = "unknown" : obj.status = obj.status.toString();
+        !obj.status ? obj.status = "Status unknown" : obj.status = obj.status.toString();
+        obj.ageGroup = getAgeGroup(obj.age);
 
         obj.id = obj.name.replace(/[^0-9a-z]/gi, '');
 
@@ -36,14 +37,14 @@ function formatData(data) {
     })
 
 
-    output.sort(function(a, b){
-    if(a.sortName < b.sortName) return -1;
-    if(a.sortName > b.sortName) return 1;
-    return 0;
-})
+    output.sort(function(a, b) {
+        if (a.sortName < b.sortName) return -1;
+        if (a.sortName > b.sortName) return 1;
+        return 0;
+    })
 
 
-    
+
 
     return output;
 }
@@ -52,7 +53,7 @@ function formatData(data) {
 
 function sortByKeys(obj) {
     let keys = Object.keys(obj),
-        i, len = keys.length;
+    i, len = keys.length;
 
     keys.sort();
 
@@ -70,33 +71,56 @@ function sortByKeys(obj) {
     return a;
 }
 
+function getAgeGroup(age) {
+    let ageGroup = "unknown";
+    if (!isNaN(age)) {
+
+        if (age < 19) { ageGroup = "18 and under" }
+        if (age > 18 && age < 35) { ageGroup = "18-34" }
+        if (age > 34 && age < 45) { ageGroup = "34-44" }
+        if (age > 44 && age < 66) { ageGroup = "45-65" }
+        if (age > 65) { ageGroup = "over 65" }
+    }
+   
+
+    return ageGroup;
+}
+
+
+function getAgeGroupStr(obj) {
+
+    
+    let ageGroup = obj.sortOn;
+    let count = obj.count;
+    let ageGroupStr = " ";
+
+        if (ageGroup == "18 and under"){ ageGroupStr = "under&nbsp;18" }
+        else if (ageGroup == "18-34" ){ ageGroupStr = "aged&nbsp;18–34" }
+        else if (ageGroup == "34-44"){ ageGroupStr = "aged&nbsp;34–44" }
+        else if (ageGroup == "45-65"){ ageGroupStr = "aged&nbsp;45–65" }
+        else if (ageGroup == "over 65"){ ageGroupStr = "over&nbsp;65" }
+        else if (ageGroup == "unknown"){ ageGroupStr = "age&nbsp;unknown" }
+    
+    console.log(ageGroupStr)
+
+    return ageGroupStr;
+}
+
+
 
 function compileHTML(dataIn) {
-
-    sections = groupBy(dataIn, 'sortOn');
-    sections = sortByKeys(sections);
-
-    var data = {
-        pageSections: sections
-    }
-
     
+    let data = dataFormatForHTML(dataIn);
 
-    var sortStr = data.pageSections[0].sortOn;
-    data.pageSections.map((obj) => {
-        var last = obj.objArr.length - 1;
-        obj.objArr[last].lastItem = true;
-
-        console.log(obj.objArr[last])
-
-    })
-    
+    Handlebars.registerHelper('html_decoder', function(text) {
+          var str = unescape(text).replace(/&amp;/g, '&');
+          return str; 
+    });
 
     Handlebars.registerPartial({
         'gridPic': gridPicTemplate,
         'detailItem': detailItemTemplate
     });
-
 
     var content = Handlebars.compile(
         mainTemplate, {
@@ -107,6 +131,54 @@ function compileHTML(dataIn) {
     var newHTML = content(data);
 
     return newHTML
+}
+
+function dataFormatForHTML(dataIn){
+    let totalCount = dataIn.length;
+    
+    sections = groupBy(dataIn, 'sortOn');
+    sections = sortByKeys(sections);
+
+    let ageGroupArr = groupBy(dataIn, 'ageGroup');
+    ageGroupArr = sortByKeys(ageGroupArr);
+    ageGroupArr.map((obj) => {
+        obj.count = obj.objArr.length; 
+        obj.ageGroupStr = getAgeGroupStr(obj);
+
+    });
+
+
+//console.log(statusArr)
+
+    let statusArr = groupBy(dataIn, "status");
+    statusArr = sortByKeys(statusArr);
+    statusArr.map((obj) => {
+        obj.count = obj.objArr.length;        
+    });
+
+
+    var data = {
+        pageSections: sections,
+        totalCount : totalCount,
+        ageGroups : ageGroupArr,
+        statusCounts : statusArr
+    }
+
+    data.pageSections.map((obj) => {
+        var last = obj.objArr.length - 1;
+        obj.objArr[last].lastItem = true;
+    })
+
+    
+
+    var sortStr = data.pageSections[0].sortOn;
+
+    data.pageSections.map((obj) => {
+        var last = obj.objArr.length - 1;
+        obj.objArr[last].lastItem = true;
+    })
+
+    return data
 }
 
 // broke this out into a function so it could also be used in the gulpfile for the image resizing
